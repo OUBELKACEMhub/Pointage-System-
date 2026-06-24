89477e5bebfce7472d40d5be5bc97f7067955caf
OUBELKACEM Ahmed <oubelkacemahmed78@gmail.com>
2026-06-24 15:48:51 +0100

    debug: show errors in start.bat windows

--- OneDrive/Documents/Pointage_Sys/start.bat
+++ OneDrive/Documents/Pointage_Sys/start.bat
@@ -2,14 +2,14 @@
 echo Demarrage ZKPointe...
 
 REM Backend Laravel
-start "ZKPointe-Backend" /min cmd /c "cd /d %~dp0backend && C:\xampp\php\php.exe artisan serve --host=0.0.0.0 --port=8000"
+start "ZKPointe-Backend" cmd /c "cd /d %~dp0backend && C:\xampp\php\php.exe artisan serve --host=0.0.0.0 --port=8000 & pause"
 
 REM Frontend
-start "ZKPointe-Frontend" /min cmd /c "node \"%APPDATA%\npm\node_modules\serve\build\main.js\" \"%~dp0frontend\dist\" -p 3000"
+start "ZKPointe-Frontend" cmd /c "node \"%APPDATA%\npm\node_modules\serve\build\main.js\" \"%~dp0frontend\dist\" -p 3000 & pause"
 
 REM Sync ZKTeco
-start "ZKPointe-Sync" /min cmd /c "cd /d %~dp0zkteco-sync && python sync.py"
+start "ZKPointe-Sync" cmd /c "cd /d %~dp0zkteco-sync && python sync.py & pause"
 
-timeout /t 4 /nobreak >nul
+timeout /t 5 /nobreak >nul
 
 start "" "http://localhost:3000"
