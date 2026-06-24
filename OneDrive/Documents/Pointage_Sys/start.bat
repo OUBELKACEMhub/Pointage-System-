063eec7d782110edf684943969fa9498e684db99
OUBELKACEM Ahmed <oubelkacemahmed78@gmail.com>
2026-06-24 14:16:32 +0100

    fix: add Python sync startup in start.bat

--- OneDrive/Documents/Pointage_Sys/start.bat
+++ OneDrive/Documents/Pointage_Sys/start.bat
@@ -1,9 +1,14 @@
 @echo off
 echo Demarrage ZKPointe...
 
-start "Backend" /min cmd /c "cd /d %~dp0backend && C:\xampp\php\php.exe artisan serve --host=0.0.0.0 --port=8000"
+REM Backend Laravel
+start "ZKPointe-Backend" /min cmd /c "cd /d %~dp0backend && C:\xampp\php\php.exe artisan serve --host=0.0.0.0 --port=8000"
 
-start "Frontend" /min cmd /c "node \"%APPDATA%\npm\node_modules\serve\build\main.js\" \"%~dp0frontend\dist\" -p 3000"
+REM Frontend
+start "ZKPointe-Frontend" /min cmd /c "node \"%APPDATA%\npm\node_modules\serve\build\main.js\" \"%~dp0frontend\dist\" -p 3000"
+
+REM Sync ZKTeco
+start "ZKPointe-Sync" /min cmd /c "cd /d %~dp0zkteco-sync && python sync.py"
 
 timeout /t 4 /nobreak >nul
 
