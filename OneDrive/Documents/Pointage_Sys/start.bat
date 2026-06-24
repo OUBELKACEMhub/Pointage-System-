1ed34428b4c5705c8bedbddd2d05990489afad3c
OUBELKACEM Ahmed <oubelkacemahmed78@gmail.com>
2026-06-24 16:15:40 +0100

    fix: use serve directly in start.bat instead of full node path

--- OneDrive/Documents/Pointage_Sys/start.bat
+++ OneDrive/Documents/Pointage_Sys/start.bat
@@ -5,7 +5,7 @@ REM Backend Laravel
 start "ZKPointe-Backend" cmd /c "cd /d %~dp0backend && C:\xampp\php\php.exe artisan serve --host=0.0.0.0 --port=8000 & pause"
 
 REM Frontend
-start "ZKPointe-Frontend" cmd /c "node \"%APPDATA%\npm\node_modules\serve\build\main.js\" \"%~dp0frontend\dist\" -p 3000 & pause"
+start "ZKPointe-Frontend" cmd /c "serve \"%~dp0frontend\dist\" -p 3000 & pause"
 
 REM Sync ZKTeco
 start "ZKPointe-Sync" cmd /c "cd /d %~dp0zkteco-sync && python sync.py & pause"
